e4803d424456296a96b74d4c2fc6613f074e1688
Somanyu <somanyu.03samal@gmail.com>
2022-11-24 14:38:18 +0530

    💄 update: Channel bar UI

--- src/components/Channelbar.jsx
+++ src/components/Channelbar.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import { BsHash } from 'react-icons/bs';
+import { FaChevronDown, FaChevronRight, FaPlus } from 'react-icons/fa';
+
+const topics = ['tailwind-css', 'react']
+const questions = ['jit-compilation', 'purge-files', 'dark-mode']
+const random = ['variants', 'plugins']
 
 function ChannelBar() {
     return (
         <div className="channel-bar shadow-lg">
-            <ChannelBlock/>
+            <ChannelBlock />
             <div className="channel-container">
-                Container-1
-                Container-2
-                Container-3
+                <Dropdown header='Topics' selections={topics} />
+                <Dropdown header='Questions' selections={questions} />
+                <Dropdown header='Random' selections={random} />
             </div>
         </div>
     )
@@ -18,5 +25,39 @@ const ChannelBlock = () => (
     </div>
 );
 
+const Dropdown = ({ header, selections }) => {
+    const [expanded, setExpanded] = useState(true);
+  
+    return (
+      <div className='dropdown'>
+        <div onClick={() => setExpanded(!expanded)} className='dropdown-header'>
+          <ChevronIcon expanded={expanded} />
+          <h5 className={expanded ? 'dropdown-header-text-selected' : 'dropdown-header-text'}>
+            {header}
+          </h5>
+          <FaPlus size='12' className='text-accent text-opacity-80 my-auto ml-auto' />
+        </div>
+        {expanded &&
+          selections &&
+          selections.map((selection) => <TopicSelection selection={selection} />)}
+      </div>
+    );
+  };
+
+const TopicSelection = ({ selection }) => (
+    <div className="dropdown-selection">
+        <BsHash size='24' className="text-gray-400" />
+        <h5 className="dropdown-selection-text">{selection}</h5>
+    </div>
+);
+
+const ChevronIcon = ({ expanded }) => {
+    const chevClass = 'text-accent text-opacity-80 my-auto mr-1';
+    return expanded ? (
+        <FaChevronDown size='14' className={chevClass} />
+    ) : (
+        <FaChevronRight size='14' className={chevClass} />
+    )
+}
 
 export default ChannelBar;
