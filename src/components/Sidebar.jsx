c2b8628f98d0a971b5e8d21148da72ede79a3edd
Somanyu <somanyu.03samal@gmail.com>
2022-11-25 08:56:00 +0530

    💄 Change FaFire -> SiDiscord

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import { BsPlus, BsFillLightningFill, BsGearFill } from 'react-icons/bs'
-import { FaFire, FaPoo } from 'react-icons/fa'
+import { FaPoo } from 'react-icons/fa'
+import { SiDiscord } from "react-icons/si";
 
 function SideBar() {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-white dark:bg-gray-900 shadow-lg">
-            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<SiDiscord size="28" />} />
             <Divider />
             <SideBarIcon icon={<BsPlus size="32" />} />
             <SideBarIcon icon={<BsFillLightningFill size="20" />} />
